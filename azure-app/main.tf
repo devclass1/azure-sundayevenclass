90bce539498a1bb7ade27b32cd5a0549bff11347
devclass1 <72622124+devclass1@users.noreply.github.com>
2023-03-19 20:23:52 +0530

    Create main.tf

---
+++ azure-app/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.48.0"
+    }
+  }
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "${var.prefix}-sp"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
+
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "${var.prefix}-example"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {
+    application_stack {
+      php_version = "8.0"
+    }
+  }
+}
+
+resource "azurerm_app_service_source_control" "example" {
+  app_id        = azurerm_linux_web_app.example.id
+  use_local_git = true
+}
