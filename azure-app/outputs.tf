105fc636b154f46a9a9998e0c1aa57207a1651ae
devclass1 <72622124+devclass1@users.noreply.github.com>
2023-03-21 22:26:14 +0530

    Create outputs.tf

---
+++ azure-app/outputs.tf
@@ -0,0 +1,13 @@
+locals {
+  scm_username = azurerm_linux_web_app.example.site_credential.0.name
+  scm_password = azurerm_linux_web_app.example.site_credential.0.password
+  repo_uri     = replace(azurerm_app_service_source_control.example.repo_url, "https://", "")
+}
+
+output "repository_url" {
+  value = "https://${local.scm_username}:${local.scm_password}@${local.repo_uri}/${azurerm_linux_web_app.example.name}.git"
+}
+
+output "app_name" {
+  value = azurerm_linux_web_app.example.default_hostname
+}
